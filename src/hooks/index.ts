46c666362b522bebc1f9000a8fd74298d67fc10e
gabrielciprianoo <devgabrielcipriano@gmail.com>
2026-03-13 11:55:29 -0600

    ✨ feat(hooks): add usePokemonPage hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,17 @@
-export { usePokemonStore } from "../store/pokemonStore";
+// Query hooks
+export { usePokemonList } from "./usePokemonList";
+export { usePokemonDetail } from "./usePokemonDetail";
+export { usePokemonsByType } from "./usePokemonsByType";
+export { usePokemonsByRegion } from "./usePokemonsByRegion";
+export { useAllPokemonNames } from "./useAllPokemonNames";
+
+// Page strategy hooks
+export { useListPage } from "./useListPage";
+export { useTypePage } from "./useTypePage";
+export { useRegionPage } from "./useRegionPage";
+export { useSearchPage } from "./useSearchPage";
+export { usePokemonPage } from "./usePokemonPage";
+export type { PokemonPage } from "./usePokemonPage";
+
+// Utilities
+export { usePageOffset } from "./usePageOffset";
